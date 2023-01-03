d004883328a9171ef54f38666bfa11e776e360c9
Valentin <5619@holbertonstudents.com>
2023-01-03 01:29:17 -0800

    task 1

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "sort.h"
+
+void insertion_sort_list(listint_t **list);
+{
+	listint_t nextnode;
+	listint_t *tmp;
+	int i = 0, y = 0;
+	int number = list->n;
+
+	while (list)
+	{
+		nextnode = list->next;
+		if (nextnode < number)
+		{
+			tmp = list;
+			while (tmp)
+			{
+				if (tmp->n < nextnode)
+				{
+					tmp->n = nextnode;
+					list->n = tmp->n;
+
+				}
+			}
+		}
+	}
