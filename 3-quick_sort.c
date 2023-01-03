a96979c759221e2e9fc9ddb3b583ac2d5bd3674e
Valentin <5619@holbertonstudents.com>
2023-01-03 06:33:49 -0800

    add task 3

---
+++ 3-quick_sort.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "sort.h"
+
+/**
+ * quick_sort - quick sort algorithm
+ * @array: array of number
+ * @size size of array
+ */
+
+void quick_sort(int *array, size_t size)
+{
+	(void) array;
+	(void) size;
+}
